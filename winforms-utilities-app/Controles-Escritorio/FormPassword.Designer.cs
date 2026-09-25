d4275d82397f12bda0f70c687560cda741ebbd3b
Your Name <your.email@example.com>
2026-09-25 13:19:36 -0300

    Feat(FormPassword): Add password validation form with security level evaluation

---
+++ winforms-utilities-app/Controles-Escritorio/FormPassword.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Controles_Escritorio
+{
+    partial class FormPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.chkMostrarPass = new System.Windows.Forms.CheckBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblInstruccion = new System.Windows.Forms.Label();
+            this.lblEvaluacion = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(38, 94);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(158, 26);
+            this.txtPassword.TabIndex = 0;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // chkMostrarPass
+            // 
+            this.chkMostrarPass.AutoSize = true;
+            this.chkMostrarPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMostrarPass.Location = new System.Drawing.Point(38, 145);
+            this.chkMostrarPass.Name = "chkMostrarPass";
+            this.chkMostrarPass.Size = new System.Drawing.Size(141, 20);
+            this.chkMostrarPass.TabIndex = 1;
+            this.chkMostrarPass.Text = "Mostrar contraseña";
+            this.chkMostrarPass.UseVisualStyleBackColor = true;
+            this.chkMostrarPass.CheckedChanged += new System.EventHandler(this.chkMostrarPass_CheckedChanged);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(248, 24);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Validador de contraseñas";
+            this.lblTitulo.Click += new System.EventHandler(this.lblEvaluacion_Click);
+            // 
+            // lblInstruccion
+            // 
+            this.lblInstruccion.AutoSize = true;
+            this.lblInstruccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruccion.Location = new System.Drawing.Point(34, 59);
+            this.lblInstruccion.Name = "lblInstruccion";
+            this.lblInstruccion.Size = new System.Drawing.Size(168, 20);
+            this.lblInstruccion.TabIndex = 3;
+            this.lblInstruccion.Text = "Ingrese su contraseña";
+            // 
+            // lblEvaluacion
+            // 
+            this.lblEvaluacion.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblEvaluacion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEvaluacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEvaluacion.Location = new System.Drawing.Point(34, 255);
+            this.lblEvaluacion.Name = "lblEvaluacion";
+            this.lblEvaluacion.Size = new System.Drawing.Size(250, 35);
+            this.lblEvaluacion.TabIndex = 4;
+            this.lblEvaluacion.Text = "\"Débil\" (o inicial)";
+            this.lblEvaluacion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(34, 219);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(142, 20);
+            this.lblInfo.TabIndex = 5;
+            this.lblInfo.Text = "Nivel de seguridad:";
+            // 
+            // FormPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(654, 450);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblEvaluacion);
+            this.Controls.Add(this.lblInstruccion);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.chkMostrarPass);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "FormPassword";
+            this.RightToLeftLayout = true;
+            this.Text = "FormPassword";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox chkMostrarPass;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblInstruccion;
+        private System.Windows.Forms.Label lblEvaluacion;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ winforms-utilities-app/Controles-Escritorio/FormPassword.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controles_Escritorio
+{
+    public partial class FormPassword : Form
+    {
+        public FormPassword()
+        {
+            InitializeComponent();
+        }
+         //ckeck largo >7
+         private bool EsLongitudValida(string pass)
+        {
+            return pass.Length > 7;
+        }
+
+        //almenos 1 digit o 1 mayuscula
+        private bool TieneNumeroYMayuscula(string pass)
+        {
+            bool tieneNumero = false;
+            bool tieneMayuscula = false;
+
+            for (int i = 0; i < pass.Length; i++)
+            {
+                char c = pass[i]; // caracter en la posicion i
+
+                if (char.IsDigit(c))
+                {
+                    tieneNumero = true;
+                }
+
+                if (char.IsUpper(c))
+                {
+                    tieneMayuscula = true;
+                }
+                if (tieneNumero && tieneMayuscula)
+                {
+                    break;
+                }
+            }
+            return tieneNumero && tieneMayuscula;
+        }
+
+        // decide si es debil,media o Fuerte
+        private string EvaluarNivelSeguridad(string pass)
+        {
+            // Caso vacío
+            if (string.IsNullOrEmpty(pass)) return "Débil";
+
+            bool longitudOk = EsLongitudValida(pass);
+            bool formatoOk = TieneNumeroYMayuscula(pass);
+
+            if (longitudOk && formatoOk)
+            {
+                return "Fuerte";
+            }
+            // al menos una de las reglas es Media
+            else if (longitudOk || formatoOk)
+            {
+                return "Media";
+            }
+            // ninguna es Debil
+            else
+            {
+                return "Débil";
+            }
+        }
+
+
+        private void lblEvaluacion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            string clave = txtPassword.Text;
+
+            // fortaleza
+            string nivel = EvaluarNivelSeguridad(clave);
+
+            // Actualizamos texto y color correspondiente
+            lblEvaluacion.Text = $"Nivel de seguridad: {nivel}";
+
+            switch (nivel)
+            {
+                case "Fuerte":
+                    lblEvaluacion.ForeColor = Color.Green;
+                    break;
+                case "Media":
+                    lblEvaluacion.ForeColor = Color.Orange;
+                    break;
+                default: // "Debil"
+                    lblEvaluacion.ForeColor = Color.Red;
+                    break;
+            }
+        }
+
+        private void chkMostrarPass_CheckedChanged(object sender, EventArgs e)
+        {
+            // Si chkMostrarPass esta marcado (muestra el texto)
+            txtPassword.UseSystemPasswordChar = !chkMostrarPass.Checked;
+        }
+    }
+}
