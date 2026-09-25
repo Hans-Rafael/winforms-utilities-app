4cc2b20f15613941a580bfd6cf8c91f679fa8903
Your Name <your.email@example.com>
2026-09-25 13:18:53 -0300

    Feat(FormCalculadora): Implement calculator functionality with discount and payment methods

---
+++ winforms-utilities-app/Controles-Escritorio/calculadoraDescuentos.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Controles_Escritorio
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.numMonto = new System.Windows.Forms.NumericUpDown();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.cmbTipoCliente = new System.Windows.Forms.ComboBox();
+            this.lblTipoCliente = new System.Windows.Forms.Label();
+            this.rbEfectivo = new System.Windows.Forms.RadioButton();
+            this.rbTarjeta = new System.Windows.Forms.RadioButton();
+            this.lblMedioPago = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblResultadoTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(150, 30);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(200, 20);
+            this.txtCliente.TabIndex = 1;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(30, 33);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(82, 13);
+            this.lblCliente.TabIndex = 0;
+            this.lblCliente.Text = "Nombre Cliente:";
+            // 
+            // numMonto
+            // 
+            this.numMonto.Location = new System.Drawing.Point(150, 70);
+            this.numMonto.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numMonto.Name = "numMonto";
+            this.numMonto.Size = new System.Drawing.Size(200, 20);
+            this.numMonto.TabIndex = 3;
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Location = new System.Drawing.Point(30, 73);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(79, 13);
+            this.lblMonto.TabIndex = 2;
+            this.lblMonto.Text = "Monto Compra:";
+            // 
+            // cmbTipoCliente
+            // 
+            this.cmbTipoCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoCliente.FormattingEnabled = true;
+            this.cmbTipoCliente.Items.AddRange(new object[] {
+            "Regular",
+            "Socio",
+            "VIP"});
+            this.cmbTipoCliente.Location = new System.Drawing.Point(150, 110);
+            this.cmbTipoCliente.Name = "cmbTipoCliente";
+            this.cmbTipoCliente.Size = new System.Drawing.Size(200, 21);
+            this.cmbTipoCliente.TabIndex = 5;
+            // 
+            // lblTipoCliente
+            // 
+            this.lblTipoCliente.AutoSize = true;
+            this.lblTipoCliente.Location = new System.Drawing.Point(30, 113);
+            this.lblTipoCliente.Name = "lblTipoCliente";
+            this.lblTipoCliente.Size = new System.Drawing.Size(81, 13);
+            this.lblTipoCliente.TabIndex = 4;
+            this.lblTipoCliente.Text = "Tipo de Cliente:";
+            // 
+            // rbEfectivo
+            // 
+            this.rbEfectivo.AutoSize = true;
+            this.rbEfectivo.Checked = true;
+            this.rbEfectivo.Location = new System.Drawing.Point(150, 150);
+            this.rbEfectivo.Name = "rbEfectivo";
+            this.rbEfectivo.Size = new System.Drawing.Size(64, 17);
+            this.rbEfectivo.TabIndex = 6;
+            this.rbEfectivo.TabStop = true;
+            this.rbEfectivo.Text = "Efectivo";
+            this.rbEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // rbTarjeta
+            // 
+            this.rbTarjeta.AutoSize = true;
+            this.rbTarjeta.Location = new System.Drawing.Point(250, 150);
+            this.rbTarjeta.Name = "rbTarjeta";
+            this.rbTarjeta.Size = new System.Drawing.Size(58, 17);
+            this.rbTarjeta.TabIndex = 7;
+            this.rbTarjeta.Text = "Tarjeta";
+            this.rbTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // lblMedioPago
+            // 
+            this.lblMedioPago.AutoSize = true;
+            this.lblMedioPago.Location = new System.Drawing.Point(30, 153);
+            this.lblMedioPago.Name = "lblMedioPago";
+            this.lblMedioPago.Size = new System.Drawing.Size(82, 13);
+            this.lblMedioPago.TabIndex = 8;
+            this.lblMedioPago.Text = "Medio de Pago:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.Location = new System.Drawing.Point(150, 200);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(200, 40);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular Total";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Location = new System.Drawing.Point(30, 290);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(320, 173);
+            this.lblResultado.TabIndex = 11;
+            this.lblResultado.Text = "Complete los datos y presione Calcular";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // lblResultadoTitulo
+            // 
+            this.lblResultadoTitulo.AutoSize = true;
+            this.lblResultadoTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoTitulo.Location = new System.Drawing.Point(30, 260);
+            this.lblResultadoTitulo.Name = "lblResultadoTitulo";
+            this.lblResultadoTitulo.Size = new System.Drawing.Size(89, 18);
+            this.lblResultadoTitulo.TabIndex = 10;
+            this.lblResultadoTitulo.Text = "Resultado:";
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 528);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblResultadoTitulo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblMedioPago);
+            this.Controls.Add(this.rbTarjeta);
+            this.Controls.Add(this.rbEfectivo);
+            this.Controls.Add(this.lblTipoCliente);
+            this.Controls.Add(this.cmbTipoCliente);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.numMonto);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.txtCliente);
+            this.Name = "FormCalculadora";
+            this.Text = "Calculadora de Descuento";
+            this.Load += new System.EventHandler(this.FormCalculadora_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numMonto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.NumericUpDown numMonto;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.ComboBox cmbTipoCliente;
+        private System.Windows.Forms.Label lblTipoCliente;
+        private System.Windows.Forms.RadioButton rbEfectivo;
+        private System.Windows.Forms.RadioButton rbTarjeta;
+        private System.Windows.Forms.Label lblMedioPago;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblResultadoTitulo;
+    }
+}
---
+++ winforms-utilities-app/Controles-Escritorio/calculadoraDescuentos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controles_Escritorio
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private decimal CalcularPorcentajeDescuento(string tipoCliente)
+        {
+            switch (tipoCliente)
+            {
+                case "Regular":
+                    return 0.0m;
+                case "Socio":
+                    return 0.10m;
+                case "VIP":
+                    return 0.20m;
+                default:
+                    return 0.0m;
+            }
+        }
+
+        private decimal CalcularRecargoMedioPago(bool esTarjeta, decimal monto)
+        {
+            if (esTarjeta)
+            {
+                return monto * 0.05m;
+            }
+            return 0;
+        }
+
+        private decimal ObtenerMontoFinal(decimal montoBase, decimal descuento, decimal recargo)
+        {
+            return montoBase - descuento + recargo;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCliente.Text))
+            {
+                MessageBox.Show("Por favor, ingrese el nombre del cliente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (numMonto.Value <= 0)
+            {
+                MessageBox.Show("Por favor, ingrese un monto válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cmbTipoCliente.SelectedIndex == -1)
+            {
+                MessageBox.Show("Por favor, seleccione el tipo de cliente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            decimal montoBase = numMonto.Value;
+            string tipoCliente = cmbTipoCliente.SelectedItem.ToString();
+            bool esTarjeta = rbTarjeta.Checked;
+
+            decimal porcentajeDescuento = CalcularPorcentajeDescuento(tipoCliente);
+            decimal descuento = montoBase * porcentajeDescuento;
+            decimal recargo = CalcularRecargoMedioPago(esTarjeta, montoBase);
+            decimal montoFinal = ObtenerMontoFinal(montoBase, descuento, recargo);
+
+            string resultado = $"Cliente: {txtCliente.Text}\n" +
+                             $"Monto Base: ${montoBase:F2}\n" +
+                             $"Tipo Cliente: {tipoCliente}\n" +
+                             $"Descuento ({porcentajeDescuento * 100}%): -${descuento:F2}\n" +
+                             $"Medio de Pago: {(esTarjeta ? "Tarjeta" : "Efectivo")}\n" +
+                             $"Recargo: ${recargo:F2}\n" +
+                             $"-----------------\n" +
+                             $"TOTAL: ${montoFinal:F2}";
+
+            lblResultado.Text = resultado;
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
